12295d3bc92c3e0f8ba4a1b91b99dd7d8607301f
Bevan Arps <bevan.arps@outlook.com>
2019-09-22 13:09:37 +1200

    Add missing using clause

--- src/Markdown.Xaml/Markdown.cs
+++ src/Markdown.Xaml/Markdown.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
